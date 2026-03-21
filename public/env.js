0d820c9fc0aacd716a61705b55b9095b8cf07069
pakwahidhas-web <pakwahidhas@gmail.com>
2026-03-21 20:17:28 +0700

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,10 +1,6 @@
-// File ini di-generate otomatis saat deploy di Vercel.
-// Nilai diisi dari Vercel Environment Variables.
-// JANGAN isi nilai asli di sini jika file ini masuk ke GitHub.
-
-window.ENV_SUPABASE_URL        = "REPLACE_SUPABASE_URL";
-window.ENV_SUPABASE_KEY        = "REPLACE_SUPABASE_ANON_KEY";
-window.ENV_ANTHROPIC_KEY       = "REPLACE_ANTHROPIC_KEY";
-window.ENV_MIDTRANS_CLIENT_KEY = "REPLACE_MIDTRANS_CLIENT_KEY";
-window.ENV_OWNER_WA            = "REPLACE_OWNER_WA";
-window.ENV_OWNER_BANK          = "REPLACE_OWNER_BANK";
+window.ENV_SUPABASE_URL        = "https://rhngvnwmnqffefdjcrgz.supabase.co";
+window.ENV_SUPABASE_KEY        = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJobmd2bndtbnFmZmVmZGpjcmd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwODg5MDEsImV4cCI6MjA4OTY2NDkwMX0.aaK--KvF2Wk90twQh-sPUuV6FWmk2LyvCcv1o_67Glw";
+window.ENV_ANTHROPIC_KEY       = "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";
+window.ENV_MIDTRANS_CLIENT_KEY = "";
+window.ENV_OWNER_WA            = "6281234567890";
+window.ENV_OWNER_BANK          = "BCA 1234567890";
