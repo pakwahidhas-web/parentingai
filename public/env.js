ed303da4c78029eacff38289589bcea5c5a26057
pakwahidhas-web <pakwahidhas@gmail.com>
2026-03-21 19:21:26 +0700

    Add files via upload

---
+++ public/env.js
@@ -0,0 +1,6 @@
+// File ini di-generate otomatis saat deploy ke Vercel.
+// JANGAN commit file ini jika berisi nilai asli.
+// Nilai diisi dari Vercel Environment Variables.
+
+window.ENV_SUPABASE_URL = "REPLACE_SUPABASE_URL";
+window.ENV_SUPABASE_KEY = "REPLACE_SUPABASE_ANON_KEY";
